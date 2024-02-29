8ba246d820f4671fd3ba646d1ff21e0723cc4e05
curiosity29 <62107278+curiosity29@users.noreply.github.com>
2024-02-29 09:15:39 +0700

    merge adapter to postprocess and fix import

--- Utils/Postprocess.py
+++ Utils/Postprocess.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from scipy.stats import multivariate_normal
+import tensorflow as tf
 def norm_kernel(size = 9):
   # Define the mean and covariance matrix
   mean = [0, 0]
@@ -22,6 +23,14 @@ def amplify(label, strength = 3):
   label = label + strength * cv2.filter2D(label, -1, norm_kernel(5)) + strength * cv2.filter2D(label, -1, norm_kernel(9)) #cv2.erode(image, np.ones(9,9),iterations = 1 )
   return label
    
+def predict_adapter(batch, model):
+  ## dilated predict
+  predict = model.predict(tf.concat([batch,batch], axis = -1), verbose = 0)[:, 1, ...]
 
+  predict = np.array([amplify(predict_, strength = 3) for predict_ in predict])
+  predict = np.argmax(predict, axis = -1)
+  predict = predict[..., np.newaxis]
+
+  return predict
   
   
