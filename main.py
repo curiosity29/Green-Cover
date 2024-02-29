c8628397307941db97e9b2a0f78eebfaf5fff84d
curiosity29 <62107278+curiosity29@users.noreply.github.com>
2024-02-29 09:03:49 +0700

    fix parameter type and add catch if file not found

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_main_args():
     arg("--image_path", type=str, default="./Images/*.tif", help="4 channel input tif file")
     arg("--save_path", type=str, default="./Predictions/prediction.tif", help="1 channel output tif file")
     arg("--batch_size", type=int, default=4, help="batch size each predict, lowering to reduce memory requirement")
-    arg("--strength", type=int, default=3, help="postprocessing parameter, amplify signal for small region")
+    arg("--strength", type=float, default=3.0, help="postprocessing parameter, amplify signal for small region")
 
     return parser.parse_args()
 
@@ -24,8 +24,12 @@ def get_main_args():
 def predict(image_path = "./image.tif", save_path = "./prediction.tif", weight_path = "./checkpoint.weights.h5", 
             batch_size = 4, strength = 3, search_path = True):
     if search_path:
-        weight_path = glob.glob(f"{weight_path}", recursive=True)[0]
-        image_path = glob.glob(f"{image_path}", recursive=True)[0]
+        try:
+            weight_path = glob.glob(f"{weight_path}", recursive=True)[0]
+            image_path = glob.glob(f"{image_path}", recursive=True)[0]
+        except:
+            print("image or weight not found")
+            return
 
     lows, highs = Configs.preprocess_get_bound()
     preprocess = partial(Preprocess.preprocess, lows = lows, highs = highs)
